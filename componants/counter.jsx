c65e3e1586a15530b8bd312a6544ffab3e65d2af
swapnilnagaonkar <nagaonkarswapnnil@gmail.com>
2019-01-01 20:24:54 +0530

    Code to delete componants

--- componants/counter.jsx
+++ componants/counter.jsx
@@ -3,7 +3,8 @@ class Counter extends React.Component {
     	count : 0,
     	tags_1 : ['tag1','tag2','tag3'],
     	tags : ['tag1','tag2','tag3'],
-        value : this.props.value,
+        value_01 : this.props.value,
+        value : this.props.counter.value,
     };
 
     krishna = {kirtan:'harinam'};
@@ -42,7 +43,11 @@ class Counter extends React.Component {
             <div>
                 {this.props.children}
                 <span className={classes} style={this.styles}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrementmy} className="btn btn-secondary btn-sm">Increment</button>              
+                <button onClick={this.handleIncrementmy} className="btn btn-secondary btn-sm">Increment</button>
+                <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.id)}>DELETE_old</button>
+
+                <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>DELETE</button>
+
             </div>
             ); 
     }
--- componants/counters.jsx
+++ componants/counters.jsx
@@ -8,10 +8,18 @@ class Counters extends React.Component {
             {id : 5 , value : 0},
         ]
     };
+
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters:counters});
+        console.log('event Handler calls',counterId);
+    }
+
     render() {    	
     	return (
     		<React.Fragment>
-                {this.state.counters.map(counter => <Counter key={counter.id} value={counter.value}  selected={true}> 
+                {this.state.counters.map(counter => <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id
+                }  selected={true} counter={counter}> 
                     <h4>Counter #{counter.id}</h4>
                 </Counter>)}
     		</React.Fragment>
