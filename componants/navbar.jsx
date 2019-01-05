39456b6eb0d1ab3f013953e8aa7f387507754dda
swapnilnagaonkar <nagaonkarswapnnil@gmail.com>
2019-01-05 09:50:20 +0530

    Stateless functional components

--- componants/navbar.jsx
+++ componants/navbar.jsx
@@ -1,3 +1,16 @@
+// StateLess functional componants
+const  NavBarSFC = (props) => {
+    return (
+        <React.Fragment>
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+            Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+            </a>
+        </nav>
+        </React.Fragment>
+    );
+}; 
+
 class NavBar extends React.Component {
     render() {    	
     	return (
@@ -10,4 +23,6 @@ class NavBar extends React.Component {
     		</React.Fragment>
     	); 
     }
+
+
 }
--- js/app.js
+++ js/app.js
@@ -38,6 +38,7 @@ class App extends React.Component {
     	return (
     		<React.Fragment>
             <NavBar  totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+            <NavBarSFC  totalCounters={this.state.counters.filter(c => c.value > 0).length} />
             <main className="container">
                 <Counters 
                 counters={this.state.counters}
